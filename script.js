769eafc7a9290b56cc647df518eab94271530ca5
kunal-arya <85951446+kunal-arya@users.noreply.github.com>
2022-07-16 22:39:38 +0530

    clock complete

---
+++ script.js
@@ -0,0 +1,24 @@
+const secondsEl = document.querySelector(".sec-hand");
+const minuteEl = document.querySelector(".min-hand");
+const hourEl = document.querySelector(".hour-hand");
+
+function getSeconds() {
+    const now = new Date();
+    const minute = now.getMinutes();
+    const seconds = now.getSeconds();
+    const hour = now.getHours();
+    const secondsDeg = ((seconds/60) * 360 ) + 90;
+    const minuteDeg  = ((minute / 60) * 360 ) + 90;
+    const hourDeg    = ((hour / 12) * 360) + 90;
+
+    if(secondsDeg >= 90 &&  secondsDeg < 91){
+        secondsEl.style.transition = `none`;
+    }
+
+    secondsEl.style.transform = `rotate(${secondsDeg}deg)`;
+    minuteEl.style.transform = `rotate(${minuteDeg}deg)`;
+    hourEl.style.transform = `rotate(${hourDeg}deg)`;
+    console.log(seconds,secondsDeg)
+}
+
+setInterval(getSeconds,1000);
